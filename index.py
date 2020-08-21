85b8ed48c963bff0f09a3fc8226975f4039133b1
karanvalooran <karanvalooran@gmail.com>
2020-08-21 23:25:44 +0530

    IndexFileAdded

---
+++ index.py
@@ -0,0 +1,44 @@
+import os
+import pyttsx3 as pt
+pt.speak("Use the following key words to start the application")
+print("Keywords : run,  open,  play,  execute,  browse,  start")
+print("for example: start chrome")
+while True:
+    pt.speak("Enter the application you would like to work with?")
+    app = input("Enter the application you would like to work with? ")
+    inp = app.lower()
+    if ("run" in inp) or ("open" in inp) or ("play" in inp) or ("execute" in inp) or ("browse" in inp) or ("start" in inp):      
+        if ("chrome" in inp):
+            pt.speak("opening chrome")
+            os.system("chrome")
+        elif "firefox" in inp:
+            pt.speak("opening firefox")
+            os.system("firefox")
+        elif "notepad" in inp:
+            pt.speak("opening notepad")
+            os.system("notepad")
+        elif "wmplayer" in inp:
+            pt.speak("opening Windows Media Player")
+            os.system("wmplayer")
+        elif "vlc" in inp:
+            pt.speak("opening V L C Media Player")
+            os.system("vlc")      
+    else:
+        pt.speak("Invalid input")
+        print("Invalid input")
+    pt.speak("Do you want me to open any other application?")
+    print("Do you want me to open any other application?")
+    pt.speak("Type yes or no to proceed")
+    print("yes or no")
+    int2=input()
+    ans=int2.lower()
+    if (ans == "yes") or (ans=="s") or (ans=="y"):
+        pt.speak("Your answer is yes")
+        continue
+    elif (ans=="no") or (ans=="n") or (ans==" "):
+        pt.speak("Your answer is no")
+        break
+pt.speak("Thank you user!")
+pt.speak("At your service!")
+exit()
+
